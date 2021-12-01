456d40aa915e79ac920da4c49593392f8e66fdb1
Matthias Bhend <matthias.bhend@students.fhnw.ch>
2021-12-01 10:36:44 +0100

    added json interface

--- webinterface/js/main.js
+++ webinterface/js/main.js
@@ -1,14 +1,5 @@
 
-function loadFile(filePath) {
-     
-    var result = null;
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", filePath, false);
-    xmlhttp.send();
-    if (xmlhttp.status==200) {
-      result = xmlhttp.responseText;
-    }
-    return result;
-  }
+import configData from './weight.json';
+document.getElementById('weight').innerHTML = JSON.parse(weight);
 
  
