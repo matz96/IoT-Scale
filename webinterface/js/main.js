489c98a0f5b8f9324bf410f928ffa228bff86515
Matthias Bhend <matthias.bhend@students.fhnw.ch>
2021-12-02 10:48:20 +0100

    changed json

--- webinterface/js/main.js
+++ webinterface/js/main.js
@@ -1,5 +1,7 @@
-import weight from "./weight.json";
+//import weight from "./weight.json";
 
-document.getElementById('weight').innerHTML = JSON.parse(weight);
+//document.getElementById('weight').innerHTML = JSON.parse(weight);
 
- 
+ document.getElementById('weight').fetch("./weight.json").then(function(resp){
+     return resp.json();
+ }).then(function(data){console.log(data);});
