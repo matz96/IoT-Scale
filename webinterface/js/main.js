43617fbd74a1b5eae59d9b88840446d6fe707bf2
Matthias Bhend <matthias.bhend@students.fhnw.ch>
2021-12-09 15:16:53 +0100

    changed source

--- webinterface/js/main.js
+++ webinterface/js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 
 // register onload function
  document.getElementById('frmFile').onload =
- readFile( "./weight.txt", "utf-8", (err,data) =>{
+ readFile( "weight.txt", "utf-8", (err,data) =>{
    if(err) throw err;
    log.console(data.toString());
  })
