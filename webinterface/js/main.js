7142204178399d94c93e93022811066cb5a7ab1c
Matthias Bhend <matthias.bhend@students.fhnw.ch>
2021-12-09 15:01:26 +0100

    changed read file

--- webinterface/js/main.js
+++ webinterface/js/main.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
     $("#div1").load("demo_test.txt");
   });
 });
-
+const fs = require('fs')
 // register onload function
  document.getElementById('frmFile').onload =
- readFile( "./weight.txt", "utf-8", (err,data) =>{
+ fs.readFile( "./weight.txt", "utf-8", (err,data) =>{
    if(err) throw err;
    log.console(data.toString());
  })
