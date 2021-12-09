49a12c4533a5b180719ee2e64dcb775f532f29cc
Matthias Bhend <matthias.bhend@students.fhnw.ch>
2021-12-09 15:04:19 +0100

    import added to top

--- webinterface/js/main.js
+++ webinterface/js/main.js
@@ -1,11 +1,11 @@
-
+import { readFile } from 'fs';
 
 $(document).ready(function(){
   $("button").click(function(){
     $("#div1").load("demo_test.txt");
   });
 });
-import { readFile } from 'fs';
+
 // register onload function
  document.getElementById('frmFile').onload =
  readFile( "./weight.txt", "utf-8", (err,data) =>{
