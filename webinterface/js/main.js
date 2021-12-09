b2c0b4a021f4c7c1a5c65f2bfb1ceb2592b2eeff
Matthias Bhend <matthias.bhend@students.fhnw.ch>
2021-12-09 14:29:50 +0100

    created new function

--- webinterface/js/main.js
+++ webinterface/js/main.js
@@ -6,18 +6,10 @@ $(document).ready(function(){
   });
 });
 
-
-function LoadFile() {
-    var oFrame = document.getElementById("frmFile");
-    var strRawContents = oFrame.contentWindow.document.body.childNodes[0].innerHTML;
-    while (strRawContents.indexOf("\r") >= 0)
-        strRawContents = strRawContents.replace("\r", "");
-    var arrLines = strRawContents.split("\n");
-    alert("File " + oFrame.src + " has " + arrLines.length + " lines");
-    for (var i = 0; i < arrLines.length; i++) {
-        var curLine = arrLines[i];
-        alert("Line #" + (i + 1) + " is: '" + curLine + "'");
-    }
-}
-
+// register onload function
+ document.getElementById('frmFile').onload = 
+ readFile( "./weight.txt", utf, (err,data) =>{
+   if(err) throw err;
+   data.toString();
+ })
 
